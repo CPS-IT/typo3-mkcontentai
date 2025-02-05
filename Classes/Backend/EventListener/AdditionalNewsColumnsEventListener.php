7a682fe0ac1bf91d8f9e8fc057e399f6e84445ba
Elias Häußler <e.haeussler@familie-redlich.de>
2025-02-05 10:25:50 +0100

    [TASK] Add additional news column to database schema

---
+++ Classes/Backend/EventListener/AdditionalNewsColumnsEventListener.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DMK\MkContentAi\Backend\EventListener;
+
+use TYPO3\CMS\Core\Database\Event\AlterTableDefinitionStatementsEvent;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+/**
+ * Add required EXT:news columns to database schema if EXT:news is installed.
+ *
+ * @author Elias Häußler <elias@haeussler.dev>
+ */
+final class AdditionalNewsColumnsEventListener
+{
+    public function __invoke(AlterTableDefinitionStatementsEvent $event): void
+    {
+        if (ExtensionManagementUtility::isLoaded('news')) {
+            $event->addSqlData(
+                $this->generateNewsSqlData(),
+            );
+        }
+    }
+
+    private function generateNewsSqlData(): string
+    {
+        return <<<SQL
+CREATE TABLE tx_news_domain_model_news
+(
+    tx_mkcontentai_original_news_uid int(11) unsigned NOT NULL DEFAULT '0'
+);
+SQL;
+    }
+}
