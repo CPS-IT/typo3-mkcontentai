e4ae4c894edf9461aad5d0d54695a8e2e46cdb48
Elias Häußler <e.haeussler@familie-redlich.de>
2025-03-20 09:01:20 +0100

    [BUGFIX] Detach news translation from original news on delete

--- Classes/Backend/Hooks/CustomDataHandler.php
+++ Classes/Backend/Hooks/CustomDataHandler.php
@@ -18,15 +18,19 @@ declare(strict_types=1);
 namespace DMK\MkContentAi\Backend\Hooks;
 
 use DMK\MkContentAi\Service\AiAltTextLogsService;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 
 class CustomDataHandler
 {
     private AiAltTextLogsService $altTextLogsService;
+    private ConnectionPool $connectionPool;
 
-    public function __construct(AiAltTextLogsService $altTextLogsService)
+    public function __construct(AiAltTextLogsService $altTextLogsService, ConnectionPool $connectionPool)
     {
         $this->altTextLogsService = $altTextLogsService;
+        $this->connectionPool = $connectionPool;
     }
 
     /**
@@ -52,5 +56,52 @@ class CustomDataHandler
 
             $this->altTextLogsService->deleteLogsByFileMetadata($fileMetadataUid, $alternativeNewMetadata);
         }
+    }
+
+    /**
+     * Detach news translation from original news if translation is deleted.
+     *
+     * This method is called right before the news translation is deleted using DataHandler.
+     * It queries the original news and, if found, detaches the translation from it.
+     */
+    public function processCmdmap_preProcess(string $command, string $table, int $id): void
+    {
+        // We only support deletions of news translations at the moment
+        if ($command !== 'delete' || $table !== 'tx_news_domain_model_news') {
+            return;
+        }
+
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('tx_news_domain_model_news');
+        $queryBuilder->getRestrictions()->removeAll();
+        $originalNews = $queryBuilder->select('tx_mkcontentai_original_news')
+            ->from('tx_news_domain_model_news')
+            ->where(
+                $queryBuilder->expr()->gt('tx_mkcontentai_original_news', 0),
+                $queryBuilder->expr()->eq('uid', $id)
+            )
+            ->execute()
+            ->fetchOne();
+
+        if ($originalNews > 0) {
+            $this->detachTranslationFromOriginalNews($originalNews);
+        }
+    }
+
+    private function detachTranslationFromOriginalNews(int $id): void
+    {
+        $connection = $this->connectionPool->getConnectionForTable('tx_news_domain_model_news');
+        $connection->update(
+            'tx_news_domain_model_news',
+            [
+                'tx_mkcontentai_translated_news' => 0,
+            ],
+            [
+                'uid' => $id,
+            ],
+            [
+                'tx_mkcontentai_translated_news' => Connection::PARAM_INT,
+                'uid' => Connection::PARAM_INT,
+            ]
+        );
     }
 }
